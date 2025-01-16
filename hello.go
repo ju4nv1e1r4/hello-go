c55fde3be6b7a5b218c53467c63bed1e7f60c7e7
juanvieira <juanvieira1@yahoo.com>
2025-01-16 10:51:10 -0300

    first "hello, world!" on golang

--- hello.go
+++ hello.go
@@ -3,5 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("===== This is my first golang script. Hello, World! =====")
+	fmt.Println("===== This is my first golang script. Hello, World! =====");
+	fmt.Println("What's next? Let's begin with basics.");
+	fmt.Println("See you all soon! o/")
 }
