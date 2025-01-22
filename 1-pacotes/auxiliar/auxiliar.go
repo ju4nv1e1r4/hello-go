61d03c6d3642f37d1a262c0ba2f33ed966c71d4a
juanvieira <juanvieira1@yahoo.com>
2025-01-22 11:46:05 -0300

    packages lessons

---
+++ 1-pacotes/auxiliar/auxiliar.go
@@ -0,0 +1,8 @@
+package auxiliar
+
+import "fmt"
+
+func Escrever() {
+	fmt.Println("Este é um arquivo auxiliar.")
+	escrever2()
+}
---
+++ 1-pacotes/auxiliar/auxiliar2.go
@@ -0,0 +1,7 @@
+package auxiliar
+
+import "fmt"
+
+func escrever2() {
+	fmt.Println("Entendendo o problema da letra minúscula. ")
+}
