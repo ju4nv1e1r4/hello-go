f6246980d2d05f823fbc2968d8e5bdb050464ab9
juanvieira <juanvieira1@yahoo.com>
2025-01-28 10:51:32 -0300

    aula sobre "herança" em go, o mais próximo desse conceito.

---
+++ 7-heranca-sqn/heranca-sqn.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+type pessoa struct {
+	nome string
+	sobrenome string
+	idade uint8
+	altura float32
+}
+
+type estudante struct 	{
+	pessoa
+	curso string
+	semestre uint8
+}
+
+func main() {
+	fmt.Println("## O mais próximo que o golang chega de uma 'Herança' ##")
+
+	p1 := pessoa{"Juan", "Vieira", 28, 1.70}
+	fmt.Println(p1)
+
+	e1 := estudante{p1, "Engenharia de Software", 1}
+	fmt.Println(e1.altura, e1.nome)
+}
